5acb734c07c9d1130b3aa63857e5182eab0f7753
jota <git@metaunicorn.com>
2017-01-11 22:00:48 +0000

    Improved dockerized environment and workflow

--- .docker/node/entrypoint.sh
+++ .docker/node/entrypoint.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 set -e
 
-cd /app
+echo "Starting node application in ${NODE_PATH}..."
+cd ${NODE_PATH}
+
+if [ ! -d "node_modules" ]
+then
+    npm install
+    if [ "install" = "${1}" ]
+    then
+        exit 0
+    fi
+fi
 
 case "$1" in
    "install") npm install
